2ce80841c2094ba1844b994a5d920a4358910eb4
mwz6259 <mwz6259@rit.edu>
2022-03-09 18:23:43 -0500

    Start Image, Label, and Button classes Import spritefont and heart sprite

---
+++ IGME-106-Group-Game/UI/Button.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IGME106GroupGame.UI
+{
+    class Button
+    {
+        // - Fields -
+        private Image image;
+        private Label label;
+
+        // - Properties -
+        /// <summary>
+        /// The x value of the button, which is also the x value of the image's button and text
+        /// </summary>
+        public int X
+        {
+            get { return image.X; }
+            set
+            {
+                image.X = value;
+                label.X = value;
+            }
+        }
+
+        /// <summary>
+        /// The y value of the button, which is also the y value of the image's button and text
+        /// </summary>
+        public int Y
+        {
+            get { return image.Y; }
+            set
+            {
+                image.Y = value;
+                label.Y = value;
+            }
+        }
+
+        // - Constructor -
+        public Button(Image image, Label label = null)
+        {
+            this.image = image;
+            this.label = label;
+        }
+
+        // - Methods -
+        /// <summary>
+        /// Draws the image and label that make up the button
+        /// </summary>
+        public void Draw(SpriteBatch sb)
+        {
+            image.Draw(sb);
+            label.Draw(sb);
+        }
+
+        /// <summary>
+        /// Determines if a given vector is intersecting with the button
+        /// </summary>
+        public bool IsIntersecting(Vector2 point)
+        {
+            Rectangle rectangle = image.CollisionBox;
+            return point.X > rectangle.X && point.X < rectangle.X + rectangle.Width
+                && point.Y > rectangle.Y && point.Y < rectangle.Y + rectangle.Height;
+        }
+    }
+}
---
+++ IGME-106-Group-Game/UI/Label.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IGME106GroupGame.UI
+{
+    class Label
+    {
+        // - Fields -
+        private string text;
+        private SpriteFont font;
+        private Vector2 position;
+        private Color tint;
+
+        // - Properties -
+        /// <summary>
+        /// The x-coordinate of the text label's position
+        /// </summary>
+        public float X
+        {
+            get { return position.X; }
+            set { position.X = value; }
+        }
+
+        /// <summary>
+        /// The y-coordinate of the text label's position
+        /// </summary>
+        public float Y
+        {
+            get { return position.Y; }
+            set { position.Y = value; }
+        }
+
+        // - Constructor -
+        public Label(string text, SpriteFont font, int x, int y, Color tint)
+        {
+            this.text = text;
+            this.font = font;
+            position = new Vector2(x, y);
+            this.tint = tint;
+        }
+
+        // - Methods -
+        /// <summary>
+        /// Draws the text label
+        /// </summary>
+        public void Draw(SpriteBatch sb)
+        {
+            sb.DrawString(font, text, position, tint);
+        }
+    }
+}
