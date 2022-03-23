0d55261a633d20672a37496ca8dd44faaa3f195c
LegendaryMaskedMan <75373849+LegendaryMaskedMan@users.noreply.github.com>
2022-03-23 17:46:46 -0400

    Copied over LevelEditor from HW2 the right way this time

---
+++ LevelEditor/LevelEditor.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace HW2
+{
+    partial class LevelEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxTileSelector = new System.Windows.Forms.GroupBox();
+            this.buttonBlack = new System.Windows.Forms.Button();
+            this.buttonBlue = new System.Windows.Forms.Button();
+            this.buttonRed = new System.Windows.Forms.Button();
+            this.buttonBrown = new System.Windows.Forms.Button();
+            this.buttonGray = new System.Windows.Forms.Button();
+            this.buttonGreen = new System.Windows.Forms.Button();
+            this.groupBoxCurrentTile = new System.Windows.Forms.GroupBox();
+            this.buttonCurrentTile = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.groupBoxMap = new System.Windows.Forms.GroupBox();
+            this.groupBoxTileSelector.SuspendLayout();
+            this.groupBoxCurrentTile.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxTileSelector
+            // 
+            this.groupBoxTileSelector.Controls.Add(this.buttonBlack);
+            this.groupBoxTileSelector.Controls.Add(this.buttonBlue);
+            this.groupBoxTileSelector.Controls.Add(this.buttonRed);
+            this.groupBoxTileSelector.Controls.Add(this.buttonBrown);
+            this.groupBoxTileSelector.Controls.Add(this.buttonGray);
+            this.groupBoxTileSelector.Controls.Add(this.buttonGreen);
+            this.groupBoxTileSelector.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxTileSelector.Name = "groupBoxTileSelector";
+            this.groupBoxTileSelector.Size = new System.Drawing.Size(107, 173);
+            this.groupBoxTileSelector.TabIndex = 0;
+            this.groupBoxTileSelector.TabStop = false;
+            this.groupBoxTileSelector.Text = "Tile Selector";
+            // 
+            // buttonBlack
+            // 
+            this.buttonBlack.BackColor = System.Drawing.Color.Black;
+            this.buttonBlack.Location = new System.Drawing.Point(57, 123);
+            this.buttonBlack.Name = "buttonBlack";
+            this.buttonBlack.Size = new System.Drawing.Size(50, 44);
+            this.buttonBlack.TabIndex = 5;
+            this.buttonBlack.UseVisualStyleBackColor = false;
+            this.buttonBlack.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // buttonBlue
+            // 
+            this.buttonBlue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonBlue.Location = new System.Drawing.Point(0, 122);
+            this.buttonBlue.Name = "buttonBlue";
+            this.buttonBlue.Size = new System.Drawing.Size(50, 44);
+            this.buttonBlue.TabIndex = 4;
+            this.buttonBlue.UseVisualStyleBackColor = false;
+            this.buttonBlue.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // buttonRed
+            // 
+            this.buttonRed.BackColor = System.Drawing.Color.Red;
+            this.buttonRed.Location = new System.Drawing.Point(57, 72);
+            this.buttonRed.Name = "buttonRed";
+            this.buttonRed.Size = new System.Drawing.Size(50, 44);
+            this.buttonRed.TabIndex = 3;
+            this.buttonRed.UseVisualStyleBackColor = false;
+            this.buttonRed.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // buttonBrown
+            // 
+            this.buttonBrown.BackColor = System.Drawing.Color.SaddleBrown;
+            this.buttonBrown.Location = new System.Drawing.Point(1, 72);
+            this.buttonBrown.Name = "buttonBrown";
+            this.buttonBrown.Size = new System.Drawing.Size(50, 44);
+            this.buttonBrown.TabIndex = 2;
+            this.buttonBrown.UseVisualStyleBackColor = false;
+            this.buttonBrown.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // buttonGray
+            // 
+            this.buttonGray.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.buttonGray.Location = new System.Drawing.Point(57, 22);
+            this.buttonGray.Name = "buttonGray";
+            this.buttonGray.Size = new System.Drawing.Size(50, 44);
+            this.buttonGray.TabIndex = 1;
+            this.buttonGray.UseVisualStyleBackColor = false;
+            this.buttonGray.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // buttonGreen
+            // 
+            this.buttonGreen.BackColor = System.Drawing.Color.Green;
+            this.buttonGreen.Location = new System.Drawing.Point(1, 22);
+            this.buttonGreen.Name = "buttonGreen";
+            this.buttonGreen.Size = new System.Drawing.Size(50, 44);
+            this.buttonGreen.TabIndex = 0;
+            this.buttonGreen.UseVisualStyleBackColor = false;
+            this.buttonGreen.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // groupBoxCurrentTile
+            // 
+            this.groupBoxCurrentTile.Controls.Add(this.buttonCurrentTile);
+            this.groupBoxCurrentTile.Location = new System.Drawing.Point(12, 191);
+            this.groupBoxCurrentTile.Name = "groupBoxCurrentTile";
+            this.groupBoxCurrentTile.Size = new System.Drawing.Size(107, 89);
+            this.groupBoxCurrentTile.TabIndex = 1;
+            this.groupBoxCurrentTile.TabStop = false;
+            this.groupBoxCurrentTile.Text = "Current Tile";
+            // 
+            // buttonCurrentTile
+            // 
+            this.buttonCurrentTile.BackColor = System.Drawing.Color.Green;
+            this.buttonCurrentTile.Location = new System.Drawing.Point(20, 22);
+            this.buttonCurrentTile.Name = "buttonCurrentTile";
+            this.buttonCurrentTile.Size = new System.Drawing.Size(66, 61);
+            this.buttonCurrentTile.TabIndex = 5;
+            this.buttonCurrentTile.UseVisualStyleBackColor = false;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonSave.Location = new System.Drawing.Point(23, 286);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(87, 73);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Save File";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonLoad.Location = new System.Drawing.Point(23, 365);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(87, 73);
+            this.buttonLoad.TabIndex = 3;
+            this.buttonLoad.Text = "Load File";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // groupBoxMap
+            // 
+            this.groupBoxMap.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxMap.Location = new System.Drawing.Point(125, 12);
+            this.groupBoxMap.Name = "groupBoxMap";
+            this.groupBoxMap.Size = new System.Drawing.Size(471, 426);
+            this.groupBoxMap.TabIndex = 4;
+            this.groupBoxMap.TabStop = false;
+            this.groupBoxMap.Text = "Map";
+            // 
+            // LevelEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(611, 450);
+            this.Controls.Add(this.groupBoxMap);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.groupBoxCurrentTile);
+            this.Controls.Add(this.groupBoxTileSelector);
+            this.Name = "LevelEditor";
+            this.Text = "Level Editor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CheckForChanges);
+            this.groupBoxTileSelector.ResumeLayout(false);
+            this.groupBoxCurrentTile.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxTileSelector;
+        private System.Windows.Forms.Button buttonBlack;
+        private System.Windows.Forms.Button buttonBlue;
+        private System.Windows.Forms.Button buttonRed;
+        private System.Windows.Forms.Button buttonBrown;
+        private System.Windows.Forms.Button buttonGray;
+        private System.Windows.Forms.Button buttonGreen;
+        private System.Windows.Forms.GroupBox groupBoxCurrentTile;
+        private System.Windows.Forms.Button buttonCurrentTile;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.GroupBox groupBoxMap;
+    }
+}
---
+++ LevelEditor/LevelEditor.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+/*-----Notes-----
+ * True tile size in the game is 60px by 60px, here it is lowered to 30px by 30x to fit the editor on the screen
+ * 
+ */
+
+namespace HW2
+{
+    public partial class LevelEditor : Form
+    {
+        // Fields
+        int mapWidth;
+        int mapHeight;
+        bool changes;
+        Form1 form;
+
+        // Properties
+        /// <summary>
+        /// This property allows the getting of the width of the map
+        /// </summary>
+        public int MapWidth { get { return mapWidth; } }
+
+        /// <summary>
+        /// This property allows the getting of the height of the map
+        /// </summary>
+        public int MapHeight { get { return mapHeight; } }
+
+        // Constructors
+        /// <summary>
+        /// This constructor will create a new LevelEditor
+        /// </summary>
+        public LevelEditor(int mapWidth, int mapHeight, Form1 form)
+        {
+            InitializeComponent();
+            this.mapWidth = mapWidth;
+            this.mapHeight = mapHeight;
+            changes = false;
+            this.form = form;
+            Size = new Size(30 * MapWidth + 175, 30 * MapHeight + 75); // temp
+            //Size = new Size(((mapWidth / mapHeight)) * 300 + 327, 489);
+        }
+
+        // Methods
+        /// <summary>
+        /// This method will set the colors of each tile to the corresponding color in the array
+        /// </summary>
+        /// <param name="colors">An array of argb ints</param>
+        public void CreateMap(int[] colors)
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for (int x = 0; x < mapWidth; x++)
+                {
+                    // Creating the PictureBox
+                    PictureBox pb = new PictureBox();
+                    //pb.Size = new Size(groupBoxMap.Size.Width / mapWidth, groupBoxMap.Size.Height / mapHeight);
+                    pb.Size = new Size(30, 30);
+
+                    // Set the color to the corresponding color in the array
+                    pb.BackColor = Color.FromArgb(colors[(y * mapWidth) + x]);
+                    
+                    // Changing the location of each PictureBox to where it should be on the window
+                    Point loc = pb.Location;
+                    loc.X = x * (pb.Width);
+                    loc.Y = y * (pb.Height);
+                    pb.Location = loc;
+
+                    // Letting the Window know the PictureBox exists
+                    this.groupBoxMap.Controls.Add(pb);
+
+                    // Hooking up the PictureBox's Click event to pictureBoxColorAssign and DetectChanges
+                    pb.Click += pictureBoxImageAssign;
+                    pb.Click += DetectChanges;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This overload of CreateMap will fill the map with a default tile for every square
+        /// </summary>
+        /// <param name="color"></param>
+        public void CreateMap(Color color) // todo: set every tile to default floor tile
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for (int x = 0; x < mapWidth; x++)
+                {
+                    // Creating the PictureBox
+                    PictureBox pb = new PictureBox();
+                    //pb.Size = new Size(groupBoxMap.Size.Width / mapWidth, groupBoxMap.Size.Height / mapHeight);
+                    pb.Size = new Size(30, 30);
+
+                    // Set the color to the corresponding color in the 
+                    pb.BackColor = color;
+
+                    // Changing the location of each PictureBox to where it should be on the window
+                    Point loc = pb.Location;
+                    loc.X = x * (pb.Width);
+                    loc.Y = y * (pb.Height);
+                    pb.Location = loc;
+
+                    // Letting the Window know the PictureBox exists
+                    this.groupBoxMap.Controls.Add(pb);
+
+                    // Hooking up the PictureBox's Click event to pictureBoxColorAssign 
+                    pb.Click += pictureBoxImageAssign;
+                    pb.Click += DetectChanges;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method will make the selected color the color that was clicked on
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonColor_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            buttonCurrentTile.BackColor = button.BackColor;
+        }
+
+        /// <summary>
+        /// This method will change the pictureBox clicked on to the selected image
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBoxImageAssign(object sender, EventArgs e)
+        {
+            PictureBox pb = (PictureBox)sender;
+            pb.Image = buttonCurrentTile.Image;
+        }
+
+        /// <summary>
+        /// This method will update the title bar with an * if changes are made
+        /// </summary>
+        private void DetectChanges(object sender, EventArgs e)
+        {
+            if (!changes)
+            {
+                this.Text += "*";
+                changes = true;
+            }
+        }
+
+        /// <summary>
+        /// This method will check if changes are not saved, and prompt the user to not quit if they want to keep their changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CheckForChanges(object sender, FormClosingEventArgs e)
+        {
+            if (changes)
+            {
+                DialogResult = MessageBox.Show("There are unsaved changes. Are you sure you want to quit?", "Unsaved Changes", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (DialogResult == DialogResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method will call LoadFile from Form1 upon click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            form.LoadFile();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            // Get all the PictureBox color values
+            int[] colors = new int[mapWidth * mapHeight];
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for (int x = 0; x < mapWidth; x++)
+                {
+                    PictureBox pb = (PictureBox)groupBoxMap.GetChildAtPoint(new Point(x * groupBoxMap.Size.Width / mapWidth, y * groupBoxMap.Size.Height / mapHeight));
+                    colors[(y * mapWidth) + x] = pb.BackColor.ToArgb();
+                }
+            }
+            // Prompt user for file location choice
+            SaveFileDialog prompt = new SaveFileDialog();
+            prompt.Filter = "Level Files|*.level";
+            prompt.Title = "Choose a place to save the file";
+            if (prompt.ShowDialog() == DialogResult.OK)
+            {
+                // Save the File
+                StreamWriter output = null;
+                try
+                {
+                    output = new StreamWriter(prompt.FileName);
+
+                    output.WriteLine($"{mapWidth},{mapHeight}");
+                    for (int i = 0; i < colors.Length; i++)
+                    {
+                        if (colors.Length - 1 == i) output.Write($"{colors[i]}");
+                        else output.Write($"{colors[i]},");
+                    }
+                    MessageBox.Show("Save successful.", "File Save Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    String[] splitDirectory = prompt.FileName.Split('\\');
+                    Text = $"Level Editor - {splitDirectory[splitDirectory.Length - 1]}";
+                    changes = false;
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    if (output != null) output.Close();
+                }
+            }
+        }  
+    }
+}
