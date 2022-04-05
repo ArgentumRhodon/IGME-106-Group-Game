7163d9273435f42bf096ff75fbe4bc83471b8224
Tonus_ <6845521-Tonus_@user.noreply.gitlab.com>
2022-04-04 22:35:51 -0400

    Prepare Assets class

---
+++ IGME-106-Group-Game/UI/Assets.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGME106GroupGame.UI
+{
+    class Assets
+    {
+        // - Fields -
+        private Game1 game;
+        private Dictionary<string, Texture2D> textures;
+
+        // - Consturctor -
+        public Assets(Game1 game)
+        {
+            this.game = game;
+            textures = new Dictionary<string, Texture2D>();
+        }
+
+        // - Methods -
+        public Texture2D Get(string key)
+        {
+            if(textures.ContainsKey(key))
+            {
+                return textures[key];
+            }
+
+            else
+            {
+                throw new KeyNotFoundException();
+            }
+        }
+
+        private void LoadContent()
+        {
+
+        }
+    }
+}
