cc112e31aebba874728eaa4d034852d6dbf741f5
Mason Zettlemoyer <mwz6259@rit.edu>
2022-04-07 16:47:08 -0400

    Alter hAlign and vAlign to use a single enum

--- IGME-106-Group-Game/UI/Menus/PauseUI.cs
+++ IGME-106-Group-Game/UI/Menus/PauseUI.cs
@@ -27,9 +27,9 @@ namespace IGME106GroupGame.UI.Menus
         /// </summary>
         public override void LoadContent()
         {
-            images.Add(new Image(game.Assets.Get("pausedTitle"), game.Graphics.PreferredBackBufferWidth / 2, game.Graphics.PreferredBackBufferHeight / 3, HAlign.Center, VAlign.Middle));
-            buttons.Add(new Button(new Image(game.Assets.Get("quitTitleText"), game.Graphics.PreferredBackBufferWidth / 2, 500, HAlign.Center, VAlign.Top), (State state) => { state.NextState = new MenuState(game); }));
-            buttons.Add(new Button(new Image(game.Assets.Get("continueText"), game.Graphics.PreferredBackBufferWidth / 2, 575, HAlign.Center, VAlign.Top), (State state) => { ((GameState)state).IsPaused = false; }));
+            images.Add(new Image(game.Assets.Get("pausedTitle"), game.Graphics.PreferredBackBufferWidth / 2, game.Graphics.PreferredBackBufferHeight / 3, Alignment.Middle, Alignment.Middle, game.Graphics));
+            buttons.Add(new Button(new Image(game.Assets.Get("quitTitleText"), game.Graphics.PreferredBackBufferWidth / 2, 500, Alignment.Middle, Alignment.Begin, game.Graphics), (State state) => { state.NextState = new MenuState(game); }));
+            buttons.Add(new Button(new Image(game.Assets.Get("continueText"), game.Graphics.PreferredBackBufferWidth / 2, 575, Alignment.Middle, Alignment.Begin, game.Graphics), (State state) => { ((GameState)state).IsPaused = false; }));
         }
     }
 }
--- IGME-106-Group-Game/UI/Menus/TitleUI.cs
+++ IGME-106-Group-Game/UI/Menus/TitleUI.cs
@@ -1,13 +1,5 @@
-﻿using LevelEditor;
+﻿using IGME106GroupGame.States;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using IGME106GroupGame.States;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace IGME106GroupGame.UI.Menus
 {
@@ -32,12 +24,12 @@ namespace IGME106GroupGame.UI.Menus
         /// </summary>
         public override void LoadContent()
         {
-            images.Add(new Image(game.Assets.Get("titleArt"), 0, 0, HAlign.Left, VAlign.Top));
-            images.Add(new Image(game.Assets.Get("titleTexture"), 0, 100, HAlign.Left, VAlign.Middle));
-            buttons.Add(new Button(new Image(game.Assets.Get("startText"), 30, 335, HAlign.Left, VAlign.Top), (State state) => { state.NextState = new GameState(game); }));
-            buttons.Add(new Button(new Image(game.Assets.Get("startAsGod"), 30, 510, HAlign.Left, VAlign.Top), (State state) => { state.NextState = new GameState(game, true); }));
-            buttons.Add(new Button(new Image(game.Assets.Get("levelEditorText"), 30, 685, HAlign.Left, VAlign.Top), (State state) => { (new LevelEditor.Form1()).Show(); }));
-            buttons.Add(new Button(new Image(game.Assets.Get("quitText"), 30, 860, HAlign.Left, VAlign.Top), (State state) => { Environment.Exit(0); }));
+            images.Add(new Image(game.Assets.Get("titleArt"), 0, 0, Alignment.Begin, Alignment.Begin, game.Graphics));
+            images.Add(new Image(game.Assets.Get("titleTexture"), 0, 50, Alignment.Middle, Alignment.Begin, game.Graphics));
+            buttons.Add(new Button(new Image(game.Assets.Get("startText"), 30, 335, Alignment.Begin, Alignment.Begin, game.Graphics), (State state) => { state.NextState = new GameState(game); }));
+            buttons.Add(new Button(new Image(game.Assets.Get("startAsGod"), 30, 510, Alignment.Begin, Alignment.Begin, game.Graphics), (State state) => { state.NextState = new GameState(game, true); }));
+            buttons.Add(new Button(new Image(game.Assets.Get("levelEditorText"), 30, 685, Alignment.Begin, Alignment.Begin, game.Graphics), (State state) => { (new LevelEditor.Form1()).Show(); }));
+            buttons.Add(new Button(new Image(game.Assets.Get("quitText"), 30, 860, Alignment.Begin, Alignment.Begin, game.Graphics), (State state) => { Environment.Exit(0); }));
         }
     }
 }
